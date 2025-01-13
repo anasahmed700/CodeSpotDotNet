deda0910ef883dd44389bf674bef307f7e771bff
Anas Ahmed <anasahmed700@gmail.com>
2025-01-13 14:26:56 +0500

    seeding roles table

--- CodeSpot/Program.cs
+++ CodeSpot/Program.cs
@@ -1,3 +1,4 @@
+using CodeSpot.Constants;
 using CodeSpot.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -26,6 +27,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    RoleSeeder.SeedRolesAsync(services).Wait();
 }
 
 app.UseHttpsRedirection();
